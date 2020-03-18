910b13c7dd43f6513d586878b53bfeec5c731685
Wiput Pootong <wiput.pootong@gmail.com>
2020-03-18 19:24:48 +0700

    :tada: initial commit

---
+++ WebServer1/build.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+docker build -t wiput1999/webserver1:latest .
+docker push wiput1999/webserver1:latest
---
+++ WebServer2/build.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+docker build -t wiput1999/webserver2:latest .
+docker push wiput1999/webserver2:latest
